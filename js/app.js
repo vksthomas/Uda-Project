91fda19473c43ec482df2027bb4c4c0765f168be
vksthomas <vksthomas@gmail.com>
2018-11-27 14:12:37 +0530

    Final Update

--- js/app.js
+++ js/app.js
@@ -5,19 +5,19 @@
 let cards =['fa-diamond','fa-diamond','fa-paper-plane-o','fa-paper-plane-o','fa-anchor','fa-anchor','fa-bolt','fa-bolt','fa-cube','fa-cube','fa-leaf',
 'fa-leaf','fa-bicycle','fa-bicycle','fa-bomb','fa-bomb'];
 
-var count = 0;
-var moves =0;
+let count = 0;
+let moves =0;
 let arraya = [];
 let count_move = 0;  
-var first_card;
-var second_card;       
-var move_var;
+let first_card;
+let second_card;       
+let move_var;
 let match_card =0;
 let t_minutes = 0;
 let t_seconds = 0;
 let star_count = 3;
 let conf = true;
-let t;
+let set_time;
 /*
  * Display the cards on the page
  *   - shuffle the list of cards using the provided "shuffle" method below*/
@@ -33,8 +33,7 @@ function shuffle(array) {
         array[currentIndex] = array[randomIndex];
         array[randomIndex] = temporaryValue;
     } 
-   var indexx = array.splice(0,1);
-    console.log(indexx);
+   let indexx = array.splice(0,1);
     return indexx;
 }
 
@@ -67,41 +66,33 @@ function toArray(eventlist) {
   }
 
 toArray(eventlist);
-//var z =shuffle(cards)[0];
+
 arrayevent.forEach(element => 
     {
 
-                //if(element.classList[1] == )
                 
-                // console.log(element.classList.map);
-            //}
             element.addEventListener('click',function()
                     {
-                            //count++;
+                            
                             if(element.classList.length == 1 && count_move !=3 && moves >= 0)
                          {
                             
                             element.classList.add("open","show");
-                                // console.log(element.classList[3]);
+                                
                                 let y = element.getElementsByClassName("fa");
-                               /* if(y[0].classList.length == 1);
-                                {
-                                   
-                                }*/
-                                //console.log(y[0].classList.length);
+                             
                                 if(y[0].classList.length === 1)
                                 {
-                                    var z = shuffle(cards)[0];
+                                    let z = shuffle(cards)[0];
                                     y[0].classList.add(z);
 
-                                  // console.log(y[0].classList[0].length);
+                                  
                                 }
-                               // console.log(y[0].classList[1]);
-                                //var classnamearr = element.className.split(" ");
+                              
                                 count++;
                                 
-                                //console.log(classnamearr);
-                                var class_check =  matchclass(y[0].classList[1],count);
+                               
+                                let class_check =  matchclass(y[0].classList[1],count);
                                 function matchclass(p,q) 
                               {
                                         if(q == 1){
@@ -119,49 +110,52 @@ arrayevent.forEach(element =>
                                         }
                     
                              }
-                                //function card_matched()
+                                
                                 if(count == 1)
                                 {
                                     first_card_match = element;
-                                   // console.log(first_card_match);
+                                 
                                 }
                                 if(count == 2)
                                 {
                                     second_card_match = element;
-                                    //console.log(second_card_match);
+                                   
                                 }
                                 if(count === 2 && class_check === true)
+                                  {    
+                                      let time = setTimeout(add_class,1000);
+                                      function add_class() 
                                         {
-                                            console.log("matched")
-                                            first_card_match.classList.add("match");
-                                            second_card_match.classList.add("match");
-                                            count = 0;
-                                            
-                                            moves = moves + 1;
-                                            move_var = document.body.getElementsByClassName("moves")[0];
-                                             move_var.innerText = moves;
-                                             match_card++;
-                                             if(match_card === 8){
-                                                 setTimeout(function(){
-                                                let conf = confirm("You Completed This Game in "+ t_seconds + " seconds and in "+moves+" moves"+"\nYou got "+star_count+" star" +"\nDo You Want to Play it again ?" );
-                                            
-                                                 if (conf == true) {
-                                                    resetFunction();
-                                                } else {
-                                                    stop_interval();
+                                                first_card_match.classList.add("match");
+                                                second_card_match.classList.add("match");
+                                                count = 0;
+                                                
+                                                moves = moves + 1;
+                                                move_var = document.body.getElementsByClassName("moves")[0];
+                                                move_var.innerText = moves;
+                                                match_card++;
+                                                if(match_card === 8){
+                                                    setTimeout(function(){
+                                                    let conf = confirm("You Completed This Game in "+ t_seconds + " seconds and in "+moves+" moves"+"\nYou got "+star_count+" star" +"\nDo You Want to Play it again ?" );
+                                                
+                                                    if (conf == true) {
+                                                        resetFunction();
+                                                    } else {
+                                                        stop_interval();
+                                                    }
+                                                    
+                                                    
+                                                    },500);
                                                 }
+                                            
                                                 
-                                                
-                                                },500);
-                                             }
+                                            }
+                                        }    
                                         
-                                            //console.log(count);
-                                        }
-                                        //console.log(count);
                                 if(count === 2 && class_check === false)
                                 {
                                          count_move = 3;   
-                                         var time = setTimeout(remove_class,1000);
+                                         let time = setTimeout(remove_class,1000);
                                             function remove_class()
                                             {
 
@@ -171,28 +165,21 @@ arrayevent.forEach(element =>
                                                         count_move = 1;
                                                         moves = moves + 1;
                                                         move_var = document.body.getElementsByClassName("moves")[0];
-                                                        console.log(move_var)
                                                         move_var.innerText = moves;
-                                                        /*var star_count = document.getElementsByClassName("stars");
-                                                        console.log(star_count);
-                                                        star_count[0].removeChild(star_count[0].childNodes[0]);
-                                                        star_count[0].removeChild(star_count[0].childNodes[0]);
-                                                       // console.log(count);*/
                                                         
                                             }
                                             count = 0;
-                                            
-                                     //console.log(count);       
+                                                 
                                 }
-                                //console.log(count);
-                                if(moves === 5 || moves === 10)
+    
+                                if(moves === 7 || moves === 10)
                                 {
-                                    var rmv_star = document.getElementsByClassName("stars");
+                                    let rmv_star = document.getElementsByClassName("stars");
                                     
                                     
                                     rmv_star[0].removeChild(rmv_star[0].childNodes[0]);
                                     star_count= rmv_star[0].childElementCount;
-                                    //star_count[0].removeChild(star_count[0].childNodes[0]);
+                                    
                                 } 
                                 setTimeout(function(){
                                     if(moves === 0){
@@ -207,9 +194,7 @@ arrayevent.forEach(element =>
 
                        
                     });
-                  //  console.log(moves)
-                    
-                        //});
+                 
          });
 
 
@@ -217,19 +202,16 @@ arrayevent.forEach(element =>
      function timer(){
         {
         
-           t = setInterval(function()
+           set_time = setInterval(function()
                 {
                         t_seconds++;
                         let t_count = document.getElementsByClassName("timer_count")[0];
-                        //let t_seconds_text = t_seconds;
-                        //console.log(t_count);
-                        t_count.innerHTML = " "+t_seconds;
-                        //console.log(t_count.innerHTML);
+                         t_count.innerHTML = " "+t_seconds;
                     },1000);
         }
      }
      function stop_interval(){
-         clearInterval(t);
+         clearInterval(set_time);
      }
      
 
@@ -239,18 +221,7 @@ arrayevent.forEach(element =>
 
 
 
-          /*while(count < 2)
-                           {  
-                               arrayevent.forEach(element => {
-                                console.log(arrayevent[0])
-                              });
-                                console.log(count);
-     
-                         }
-                         //console.log(element);
-                         //element.removeEventListener("click",function(){},true);*/
-//});
- 
+         
 // Shuffle function from http://stackoverflow.com/a/2450976
 
 
@@ -267,147 +238,4 @@ arrayevent.forEach(element =>
  *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- */
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- /*
- function generateCard(Card){
-    return `li class="card"><i class="fa ${card}"></i></li>`;
-}
- function initGame(){
-    var deck = document.querySelector('.deck');
-    var cardHTML= shuffle(cards).map(function(card){
-        return generateCard(card);
-    });
-    moves = 0;
-    deck.innerHTML= cardHTML.join('');
- }
-
- var allCards = document.querySelectorAll(".card");
- var openCards = [];
- var moves = 0;
- var moveCounter = document.querySelector('.moves');
-
-
- initGame();
-
-
- 
-
- allCards.forEach(function(card){
-    card.addEventListener('click',function(e){
-
-       if (!card.classList.contains('open') && !card.classList.contains('show'))
-
-       openCards.push(card);
-       card.classList.add('open','show');
-
-       
-        if(openCards.length == 2){
-            if(openCards[0].dataset.card == openCards[1].dataset.card){
-               openCards[0].classList.add("match");
-               openCards[0].classList.add("open");
-               openCards[0].classList.add("show");
-
-
-            }else{
-                    
-                    setTimeout(function(){
-                        openCards.forEach(function(card){
-                            cards.classList.remove('open','show');
-
-                        });
-                        openCards= [];
-                    }, 1000);
-                }
-           moves += 1;
-           moveCounter.inner
-        }     
-    })
-
- });  
-
-
-
- ------------------------------
-
- let arrayEventItems = toArray(eventlist);
-//var z =shuffle(cards)[0];
-arrayEventItems.forEach(element => {
-  element.addEventListener('click',function()
-                {
-                    if(element.className === 'openedItem'){
-                        console.log(element)
-                        element.removeEventListener("click",function(){},true);
-                    } 
-                    else {
-                        element.classList.add("open","show");
-                        let childNodes = toArray(element.childNodes);
-                        // console.log(childNodes)
-                        // childNodes[0].add('openedItem');
-                        let y = element.getElementsByClassName("fa");
-                        let z = shuffle(cards)[0];
-                        console.log(z);
-                        y[0].classList.add(z);
-                        while(count<2)
-                            {  arraya.forEach(element => {
-                                    arraya[element] = y[0];
-                                console.log(arraya[element]);
-                                });
-                                count++;
-                                console.log(count);
-
-                            }
-                        }
-                });
-                console.log(element);
-         });
-
  */
